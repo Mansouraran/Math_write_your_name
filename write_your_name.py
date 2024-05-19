d9c9be75214dd4cfdb7f70ed5d0b08ef15037408
Mansour Salari <75796316+Mansouraran@users.noreply.github.com>
2024-05-19 01:49:07 -0400

    Update write_your_name.py
    update_3

--- write_your_name.py
+++ write_your_name.py
@@ -1,10 +1,11 @@
 def hi_my_name_is():
-    '''
+    """
     it returns the name of user
 
     Returns:
         str: your name.
-    '''
+    """
+    
     return "hi, Mansour Salari"
 
 if __name__ == "__main__":
